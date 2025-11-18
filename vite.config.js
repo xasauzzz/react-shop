4fde3f5cc49b2962aa237e0770ef66db0c214a6a
Timur Azimov <xasauzzz@users.noreply.github.com>
2025-11-18 22:51:00 +0500

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/react-sneakers/',
   plugins: [react()],
+  base: '/react-shop/',
+  build: {
+    outDir: 'docs',
+  },
 })
+
+
